a845fba0c266bfb29d3d0ac4b44223226306544c
Octavio Marchi <octaviomarchi@msn.com>
2020-09-03 20:36:52 -0300

    debugger + deals won retornados

--- src/controllers/pipedrive-controller.js
+++ src/controllers/pipedrive-controller.js
@@ -1,8 +1,10 @@
 const pipedrive = require("../integrations/pipedrive");
 
 exports.getDeals = async (req, res, next) => {
+  const parameters = {status: 'won'};
+
   try {
-    const data = await pipedrive.getAllDeals();
+    const data = await pipedrive.getDeals(parameters);
     res.status(200).send(data);
   } catch (error) {
     res.status(500).send({ error: error });
--- src/integrations/pipedrive.js
+++ src/integrations/pipedrive.js
@@ -4,8 +4,12 @@ const config = require("../config");
 pipedrive.Configuration.apiToken = config.pipedriveToken;
 
 exports.getAllDeals = async () => {
-  return await pipedrive.DealsController.getAllDeals({});
+  return await pipedrive.DealsController.getAllDeals();
 };
+
+exports.getDeals = async (parameters) => {
+  return await pipedrive.DealsController.getAllDeals(parameters);
+}
 
 exports.getUserData = async () => {
   return await pipedrive.UsersController.getCurrentUserData();
