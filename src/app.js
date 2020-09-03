b30b5719864f6ad898929c26ddd2d3340da6ef6b
Octavio Marchi <octaviomarchi@msn.com>
2020-09-03 17:37:41 -0300

    app init
    npm init com algumas pastas base e conexão do banco

---
+++ src/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const config = require("./config");
+
+const app = express();
+const router = express.Router();
+
+// Conecta ao banco
+mongoose
+  .connect(config.mongoConnectionString, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB Connected!"))
+  .catch((err) => {
+    console.log("DB Connection Error: ${err.message}");
+  });
+
+//Carrega as rotas
+const indexRoute = require("./routes/index-route");
+
+app.use(
+  bodyParser.json({
+    limit: "5mb",
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+// Habilita o CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-access-token"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.use("/", indexRoute);
+
+module.exports = app;
---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoConnectionString:
+    "mongodb+srv://admin:LgWLf50m2Zc0aQ8x@linkapi-test.qb3el.gcp.mongodb.net/LinkAPI-Test?retryWrites=true&w=majority",
+};
